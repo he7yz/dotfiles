d525b691b12af5cde9edec4739aeba7fe5369bb3
he7yz <shazarulhafiz7@gmail.com>
2026-05-18 03:59:53 +0800

    feat: configs for colorizer and cord.nvim

---
+++ lazyvim/lua/config/cord-config.lua
@@ -0,0 +1,14 @@
+return {
+  'vyfor/cord.nvim',
+  ---@type CordConfig
+  opts = {
+    idle = {
+      enabled = false,
+    },
+    -- RPC appearance
+    display = {
+      theme = 'default',
+      flavor = 'dark',
+    },
+  },
+}
